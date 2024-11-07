87fdfdc7e5ad21d6ee52be43dcf48ccf967e2592
Railan Santana <lansilvah14fsa@gmail.com>
2024-11-07 11:21:28 -0300

    chore(loader): add react-spinners

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import SyncLoader from 'react-spinners/SyncLoader';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -128,7 +129,7 @@ const App = () => {
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      {loading && <p>Carregando...</p>}
+      {loading && <SyncLoader/>}
       <form onSubmit={handleSubmit}>
         <input
           type="text"
